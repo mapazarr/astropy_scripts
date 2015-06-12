fa97598f3dea0f543156265bf50b37f825336cbb
mapaz <mapazarr@users.noreply.github.com>
2015-06-12 07:36:05 -0700

    Added a few more tests (asserts) for make_test_obs_table.

--- astropy_scripts/test_make_test_obs_table.py
+++ astropy_scripts/test_make_test_obs_table.py
@@ -2,6 +2,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals) # python 2 as python 3
 import numpy as np
 from astropy.table import Table, Column
+from astropy.coordinates import Angle
 from gammapy.datasets.make import make_test_observation_table
 from gammapy.obs import ObservationTable
 
@@ -37,7 +38,25 @@ print(obs_table)
 
 print()
 
-#test: assert if the length of the table is n_obs:
+# test: assert if the length of the table is n_obs:
 print("obs_table length")
 print(len(obs_table))
 assert len(obs_table) == n_obs
+
+# test: assert if the TIME_START > 0:
+assert (obs_table['TIME_START'] > 0).all()
+
+# test: assert if TIME_STOP > TIME_START:
+assert (obs_table['TIME_STOP'] > obs_table['TIME_START']).all()
+
+# test: assert if RA is in the interval (0, 360) deg:
+ra_min = Angle(0, 'degree')
+ra_max = Angle(360, 'degree')
+assert (ra_min < obs_table['RA']).all()
+assert (obs_table['RA'] < ra_max).all()
+
+# test: assert if dec is inthe interval (-90, 90) deg:
+dec_min = Angle(-90, 'degree')
+dec_max = Angle(90, 'degree')
+assert (dec_min < obs_table['DEC']).all()
+assert (obs_table['DEC'] < dec_max).all()
