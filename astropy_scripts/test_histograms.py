8c6242955e4ae29ab2d295004124b0cc16c2090e
mapaz <mapazarr@users.noreply.github.com>
2015-06-15 16:58:39 -0700

    Working with colum-row transformations and with astropy tables.

--- astropy_scripts/test_histograms.py
+++ astropy_scripts/test_histograms.py
@@ -1,6 +1,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals) # python 2 as python 3
 import numpy as np
+from astropy.table import Table
 
 data = np.array([1.,2.,3.,4.,5.])
 print("data", repr(data))
@@ -13,8 +14,9 @@ data_x = np.array([1.,2.,3.,4.,5.]) # column-wise # works for histogram2d
 data_y = np.array([11.,12.,13.,14.,15.]) # works for histogram2d
 print("data_x", repr(data_x))
 print("data_y", repr(data_y))
-#data_2d = np.array([[1.,2.,3.,4.,5.], [11.,12.,13.,14.,15.]]) # column-wise # doesn't work!
-data_2d = np.array([[1.,11.], [2.,12.], [3.,13.], [4.,14.], [5.,15.]]) # row-wise # works for histogramdd
+data_2d = np.array([[1.,2.,3.,4.,5.], [11.,12.,13.,14.,15.]]) # column-wise # doesn't work!
+data_2d_col = np.array([[1.,2.,3.,4.,5.], [11.,12.,13.,14.,15.]]) # column-wise # doesn't work!
+data_2d_row = np.array([[1.,11.], [2.,12.], [3.,13.], [4.,14.], [5.,15.]]) # row-wise # works for histogramdd
 print("data_2d", repr(data_2d))
 #bins_2d = np.array([[0., 2.5, 5.], [10., 12.5, 15.]])
 bins_2d = np.array([[0., 2., 4., 6.], [10., 12., 14., 16.]])
@@ -23,7 +25,21 @@ print("bins_2d", repr(bins_2d))
 #np.histogram(data_2d, bins_2d) # doesn't work!
 #np.histogram2d(data_2d, bins_2d) # doesn't work!
 h2d_data = np.histogram2d(data_x, data_y, bins_2d)
-hdd_data = np.histogramdd(data_2d, bins_2d)
+hdd_data = np.histogramdd(data_2d_row, bins_2d)
+
+# convert column-wise data to row-wise data
+data_2d_col_trans = np.transpose(data_2d_col)
+
+# test astropy table
+astro_table = Table()
+astro_table['X'] = data_x
+astro_table['Y'] = data_y
+print("astro_table")
+print(astro_table)
+data = np.array(astro_table)
+print("data", data)
+#hdd_table = np.histogramdd(astro_table, bins_2d)
+#hdd_table = np.histogramdd(data, bins_2d)
 
 import IPython; IPython.embed()
 dig_data
