270a8fa96e96923df769f66b5cc19c4b3535b7fe
mapaz <mapazarr@users.noreply.github.com>
2015-06-29 16:20:07 -0700

    Testing quantities and angles, and zenith angle.

--- astropy_scripts/test_filter_observations.py
+++ astropy_scripts/test_filter_observations.py
@@ -1,6 +1,8 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals) # python 2 as python 3
 import numpy as np
+from astropy.units import Quantity
+from astropy.time import Time
 from astropy.coordinates import Angle
 import gammapy
 from gammapy.obs import ObservationTable
@@ -43,35 +45,35 @@ def test_filter_observations():
     print()
     print("Test box selection in OBS_ID")
     variable = 'OBS_ID'
-    min = 2
-    max = 5
+    value_min = 2
+    value_max = 5
     selection = dict(shape='box', variable=variable,
-                     min=min, max=max)
+                     value_min=value_min, value_max=value_max)
     filtered_obs_table = obs_table.filter_observations(selection)
     print("obs_table")
     print(obs_table)
     print("filtered_obs_table")
     print(filtered_obs_table)
-    assert len(filtered_obs_table) == 3
-    assert (min <= filtered_obs_table[variable]).all()
-    assert (filtered_obs_table[variable] < max).all()
+    assert len(filtered_obs_table) == 2
+    assert (value_min < filtered_obs_table[variable]).all()
+    assert (filtered_obs_table[variable] < value_max).all()
 
     # test box selection in obs_id inverted
     print()
     print("Test box selection in OBS_ID inverted")
     variable = 'OBS_ID'
-    min = 2
-    max = 5
+    value_min = 2
+    value_max = 5
     selection = dict(shape='box', variable=variable,
-                     min=min, max=max, inverted=True)
+                     value_min=value_min, value_max=value_max, inverted=True)
     filtered_obs_table = obs_table.filter_observations(selection)
     print("obs_table")
     print(obs_table)
     print("filtered_obs_table")
     print(filtered_obs_table)
-    assert len(filtered_obs_table) == 7
-    assert ((min > filtered_obs_table[variable]) |
-            (filtered_obs_table[variable] >= max)).all()
+    assert len(filtered_obs_table) == 8
+    assert ((value_min >= filtered_obs_table[variable]) |
+            (filtered_obs_table[variable] >= value_max)).all()
 
     # test circle selection in obs_id
     print()
@@ -86,9 +88,55 @@ def test_filter_observations():
     print(obs_table)
     print("filtered_obs_table")
     print(filtered_obs_table)
-    assert len(filtered_obs_table) == 4
-    assert (center - radius <= filtered_obs_table[variable]).all()
+    assert len(filtered_obs_table) == 3
+    assert (center - radius < filtered_obs_table[variable]).all()
     assert (filtered_obs_table[variable] < center + radius).all()
+
+    ##TEST OTHER VARIABLES !!!! (like quantites or angles...)
+
+    # test box selection in alt
+    print()
+    print("Test box selection in ALT")
+    variable = 'ALT'
+    #value_min = 70.
+    #value_max = 80.
+    #value_min = Angle(70., 'degree')
+    #value_max = Angle(80., 'degree')
+    value_min = Angle(Angle(70., 'degree').radian, 'radian')
+    value_max = Angle(Angle(80., 'degree').radian, 'radian')
+    selection = dict(shape='box', variable=variable,
+                     value_min=value_min, value_max=value_max)
+    filtered_obs_table = obs_table.filter_observations(selection)
+    print("obs_table")
+    print(obs_table)
+    print("filtered_obs_table")
+    print(filtered_obs_table)
+    #assert (value_min < Quantity(filtered_obs_table[variable])).all()
+    #assert (Quantity(filtered_obs_table[variable]) < value_max).all()
+    assert (value_min < Angle(filtered_obs_table[variable])).all()
+    assert (Angle(filtered_obs_table[variable]) < value_max).all()
+
+    # test box selection in zenith angle
+    print()
+    print("Test box selection in zenith")
+    variable = 'zenith'
+    value_min = Angle(20., 'degree')
+    value_max = Angle(30., 'degree')
+    selection = dict(shape='box', variable=variable,
+                     value_min=value_min, value_max=value_max)
+    filtered_obs_table = obs_table.filter_observations(selection)
+    print("obs_table")
+    print(obs_table)
+    print("filtered_obs_table")
+    print(filtered_obs_table)
+    zenith = Angle(90., 'degree') - filtered_obs_table['ALT']
+    print("zenit = {}".format(zenith))
+    assert (value_min < zenith).all()
+    assert (zenith < value_max).all()
+
+    # test box selection in time_start
+
+    # test box selection in time: (tstart, tstop) within (time_min, time_max)
 
     return
 
