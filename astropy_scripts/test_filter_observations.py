92ee30064a304c71516122d92eea0c41c0958ab2
mapaz <mapazarr@users.noreply.github.com>
2015-07-01 00:04:31 -0700

    Finished tests for the time filters.

--- astropy_scripts/test_filter_observations.py
+++ astropy_scripts/test_filter_observations.py
@@ -2,11 +2,11 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals) # python 2 as python 3
 import numpy as np
 from astropy.units import Quantity
-from astropy.time import Time
 from astropy.coordinates import Angle
-import gammapy
+from astropy.time import Time
 from gammapy.obs import ObservationTable
 from gammapy.datasets.make import make_test_observation_table
+from gammapy.time import absolute_time
 
 def test_filter_observations():
     # create random observation table
@@ -61,9 +61,9 @@ def test_filter_observations():
     # test box selection in obs_id inverted
     print()
     print("Test box selection in OBS_ID inverted")
-    variable = 'OBS_ID'
-    value_min = 2
-    value_max = 5
+    #variable = 'OBS_ID'
+    #value_min = 2
+    #value_max = 5
     selection = dict(shape='box', variable=variable,
                      value_min=value_min, value_max=value_max, inverted=True)
     filtered_obs_table = obs_table.filter_observations(selection)
@@ -91,8 +91,6 @@ def test_filter_observations():
     assert len(filtered_obs_table) == 3
     assert (center - radius < filtered_obs_table[variable]).all()
     assert (filtered_obs_table[variable] < center + radius).all()
-
-    ##TEST OTHER VARIABLES !!!! (like quantites or angles...)
 
     # test box selection in alt
     print()
@@ -135,10 +133,59 @@ def test_filter_observations():
     assert (zenith < value_max).all()
 
     # test box selection in time_start
+    print()
+    print("Test box selection in TIME_START")
+    variable = 'TIME_START'
+    value_min = Time('2012-01-01 00:00:00', format='iso', scale='utc')
+    value_max = Time('2014-01-01 00:00:00', format='iso', scale='utc')
+    selection = dict(shape='box', variable=variable,
+                     value_min=value_min, value_max=value_max)
+    filtered_obs_table = obs_table.filter_observations(selection)
+    print("obs_table")
+    print(obs_table)
+    print("filtered_obs_table")
+    print(filtered_obs_table)
+    time_start = absolute_time(filtered_obs_table['TIME_START'], filtered_obs_table.meta)
+    print("time_start = {}".format(repr(time_start)))
+    assert (value_min < time_start).all()
+    assert (time_start < value_max).all()
 
-    # test box selection in time: (tstart, tstop) within (time_min, time_max)
+    # test box selection in time: (time_start, time_stop) within (value_min, value_min)
+    print()
+    print("Test box selection in time")
+    # new obs table with very close (in time) observations (and times in absolute times)
+    datestart = Time('2012-01-01 00:03:00', format='iso', scale='utc')
+    dateend = Time('2012-01-01 02:03:00', format='iso', scale='utc')
+    obs_table_time = make_test_observation_table(observatory_name, n_obs,
+                                            datestart, dateend, True)
+    variable = 'time'
+    value_min = Time('2012-01-01 01:00:00', format='iso', scale='utc')
+    value_max = Time('2012-01-01 02:00:00', format='iso', scale='utc')
+    selection = dict(shape='box', variable=variable,
+                     value_min=value_min, value_max=value_max)
+    filtered_obs_table = obs_table_time.filter_observations(selection)
+    print("obs_table_time")
+    print(obs_table_time)
+    print("filtered_obs_table")
+    print(filtered_obs_table)
+    if filtered_obs_table.meta['TIME_FORMAT'] == 'absolute': # this is true, since I asked for it when creating the obs table
+        time_start = filtered_obs_table['TIME_START']
+        time_stop = filtered_obs_table['TIME_STOP']
+    else:
+        time_start = absolute_time(filtered_obs_table['TIME_START'], filtered_obs_table.meta)
+        time_stop = absolute_time(filtered_obs_table['TIME_STOP'], filtered_obs_table.meta)
+    print("time_start = {}".format(repr(time_start)))
+    print("time_stop = {}".format(repr(time_stop)))
+    assert (value_min < time_start).all()
+    assert (time_start < value_max).all()
+    assert (value_min < time_stop).all()
+    assert (time_stop < value_max).all()
 
     return
+
+
+    # TODO: continue with the sky_box/sky_circle tests!!!!
+
 
     # test sky box selection in gal coordinates
     print()
@@ -156,9 +203,8 @@ def test_filter_observations():
     print(filtered_obs_table)
 
 
-    # TODO: more!!! (implement some filters and test them!!!)
 
-    # TODO: implement "inverse" filter: keep runs not inside, but outside selection criteria!!!
+
 
 if __name__ == '__main__':
     test_filter_observations()
