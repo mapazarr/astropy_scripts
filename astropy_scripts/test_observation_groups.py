eea88ab84c24eddc1c7312ce11c46a227bfd0f63
mapaz <mapazarr@users.noreply.github.com>
2015-08-13 16:17:38 -0700

    Working on observation groups.

--- astropy_scripts/test_observation_groups.py
+++ astropy_scripts/test_observation_groups.py
@@ -2,6 +2,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals) # python 2 as python 3
 import numpy as np
 from astropy.coordinates import Angle
+from astropy.table import Table
 from gammapy.obs import ObservationGroups, ObservationGroupAxis
 
 alt = Angle([0, 30, 60, 90], 'degree')
@@ -14,58 +15,62 @@ ntels_obs_group_axis = ObservationGroupAxis('N_TELS', ntels, 'bin_values')
 
 print("ALT n_bins", alt_obs_group_axis.n_bins)
 print("ALT bin 2", alt_obs_group_axis.get_bin(2))
-##print("ALT bins", alt_obs_group_axis.get_bins)
+print("ALT bins", alt_obs_group_axis.get_bins)
+print("ALT col:", alt_obs_group_axis.to_column().meta)
+print(alt_obs_group_axis.to_column())
+
+alt_obs_group_axis_from_col = ObservationGroupAxis.from_column(alt_obs_group_axis.to_column())
+print("ALT from col col:", alt_obs_group_axis_from_col.to_column().meta)
+print(alt_obs_group_axis_from_col.to_column())
+print("ALT from col bins", alt_obs_group_axis.bins)
+
+print()
+alt_obs_group_axis.print() # not working?
+az_obs_group_axis.print() # not working?
+ntels_obs_group_axis.print() # not working?
+print(alt_obs_group_axis.info)
+print(az_obs_group_axis.info)
+print(ntels_obs_group_axis.info)
 
 list_obs_group_axis = [alt_obs_group_axis, az_obs_group_axis, ntels_obs_group_axis]
 array_obs_group_axis = np.array([alt_obs_group_axis, az_obs_group_axis, ntels_obs_group_axis])
 
 obs_group = ObservationGroups(list_obs_group_axis)
 
+obs_group.print_groups() # works?
+obs_group.print_axes() # not working?
+print(obs_group.info)
+print(obs_group.obs_groups_table)
+
+# write
+obs_group.write('obs_groups.ecsv')
+
+# read
+obs_group = ObservationGroups.read('obs_groups.ecsv', format='obs_groups_def')
+
+#import IPython; IPython.embed()
+
+print(obs_group.obs_groups_table)
+print(obs_group.info)
 
 
+# test columns of different lengths:
+# TODO: this is an attempt for a function to pass observation axis definitions from/to a text (ECSV) file.
+# It is not working because of different lengths of the arrays; possibilities:
+# 1) define to_row/from_row in ObservationGroupAxis, an pack the info in a table of columns: name, unit, format, bins (the latter should be an array of either bin values or bin edges: np.array(bins))
+# 2) Play with masked tables: define all columns with the same number of bins, then mask the ones that do not proceed.
+# Possibility 1 can be stored in a regular CSV file, maybe?
 
-##import IPython; IPython.embed()
-
-
-# test the definition of observation groups
-
-#array_ob_bins = np.array([])
+#axis_table = Table(masked=True)
 #for i_axis in list_obs_group_axis:
-#    array_ob_bins.append(np.arange(len(list_obs_group_axis[i_axis])))
+#    #axis_table[i_axis.name] = i_axis.bins
+#    axis_table.add_column(i_axis.to_column())
+#    axis_table.meta[i_axis.name + "_axis_format"] = i_axis.format
+#    axis_table.meta[i_axis.name + "_axis_format"] = i_axis.format
+#    print(axis_table)
+#    print(axis_table.meta)
+#print()
+#print(axis_table.meta)
+#print(axis_table)
 
-#list_of_bins = []
-#for i_axis in np.arange(len(list_obs_group_axis)):
-#    list_of_bins.append(np.arange(len(list_obs_group_axis[i_axis].bins)))
-
-array_of_bins = np.array([])
-for i_axis in np.arange(len(list_obs_group_axis)):
-    array_of_bins = np.append(array_of_bins, np.arange(len(list_obs_group_axis[i_axis].bins)))
-
-##import IPython; IPython.embed()
-
-
-stuff = [[0, 30, 60, 90], [-90, 90, 270], [3, 4]]
-stuff = np.array([[0, 30, 60, 90], [-90, 90, 270], [3, 4]])
-stuff = np.array([np.array([0, 30, 60, 90]), np.array([-90, 90, 270]), np.array([3, 4])])
-
-grid = np.meshgrid(stuff, indexing='ij')
-
-grid = np.meshgrid(np.array([0, 30, 60, 90]), np.array([-90, 90, 270]), np.array([3, 4]), indexing='ij')
-
-len(grid)
-
-##import IPython; IPython.embed()
-
-
-
-
-a = np.array([11, 12])
-b = np.array([21, 22])
-a
-b
-a_ab, b_ab = np.meshgrid(a, b, indexing='ij')
-a_ab
-b_ab
-
-c = np.array([31, 32])
-
+print('Done.')
