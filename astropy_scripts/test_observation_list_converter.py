2a8e847cb61b80746e27bf2e4ac64484169fc466
mapaz <mapazarr@users.noreply.github.com>
2015-08-15 07:16:55 -0700

    Added function to test obs table format converter.

--- astropy_scripts/test_observation_list_converter.py
+++ astropy_scripts/test_observation_list_converter.py
@@ -1,15 +1,15 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals) # python 2 as python 3
 from astropy.table import Table
-from gammapy.datasets import (convert_obs_list_format_to_gammapy,
-                              convert_obs_list_hess_to_gammapy)
-##from gammapy.obs import DataStore
+from gammapy.obs import (convert_obs_list_format_to_gammapy,
+                         convert_obs_list_hess_to_gammapy,
+                         DataStore)
 
 # test: convert the H.E.S.S. runinfo.fits observation list
 # from the PA FITS production
 
-inputdir = '/home/mapaz/HESS/fits_data/pa_fits_prod02/pa/Model_Deconvoluted_Prod26/Mpp_Std/'
-infile = inputdir + 'runinfo.fits'
+fits_path = '/home/mapaz/HESS/fits_data/pa_fits_prod02/pa/Model_Deconvoluted_Prod26/Mpp_Std/'
+infile = fits_path + 'runinfo.fits'
 
 obs_list = Table.read(infile)
 
@@ -36,25 +36,25 @@ print(obs_table)
 
 # test how the datastore classes handle the converter
 
-###data_store = DataStore(dir=fits_path)
-###obs_table = data_store.make_observation_table()
-###print()
-###print("obs_table")
-###print(obs_table.meta)
-###print(obs_table)
-###
-###event_list_files = data_store.make_table_of_files(observation_table, filetypes=['events'])
-###aeff_list_files = data_store.make_table_of_files(observation_table, filetypes=['effective area'])
+# if I try to import the converters in datastore.py, I get errors
+# I think it is a circular dependency (I also can't import make_test_observation_table in there for instance
+# moving the converters (convert.py) from datasets to obs didn't help
+# I am updating the corresponding __init__.py accordingly...
+# moving the functions to observation.py seems to work
 
+data_store = DataStore(dir=fits_path)
+obs_table = data_store.make_observation_table()
+print()
+print("obs_table datastore")
+print(obs_table.meta)
+print(obs_table)
 
+event_list_files = data_store.make_table_of_files(obs_table, filetypes=['events'])
+print()
+print("event_list_files")
+print(event_list_files)
 
-#import IPython; IPython.embed()
-
-
-# TODO: I probably have to "update" gammapy/obs/datastore.py to use the converter!!!
-# DataStore.__init__ defines the file name
-# DataStoreIndexTable.read is the one reading the file
-# DataStoreIndexTable._init_cleanup -> review!!!
-# check other TODOs within this PR (i.e. glon glat no units, etc)
-#
-# does runinfo have units??? or does the  DataStore/DataStoreIndexTable classes add them???!!!
+aeff_list_files = data_store.make_table_of_files(obs_table, filetypes=['effective area'])
+print()
+print("aeff_list_files")
+print(aeff_list_files)
